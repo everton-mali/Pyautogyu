d71bb0b87d4eaec2bc60b50576e4db0099cdd4ba
Everton Malinoski <81785184+everton-mali@users.noreply.github.com>
2023-02-10 11:55:07 -0300

    Add files via upload
    import pyautogui
    import time
    import threading
    
    
    pyautogui.moveTo(519,235)
    pyautogui.leftClick()
    pyautogui.press('down')
    pyautogui.press('space')
    pyautogui.press('space')
    pyautogui.press('down')
    pyautogui.press('space')
    pyautogui.press('space')
    pyautogui.press('down')
    pyautogui.press('space')
    pyautogui.press('space')
    pyautogui.press('down')
    pyautogui.press('space')
    pyautogui.press('space')
    time.sleep(1)
    pyautogui.moveTo(796, 439)
    pyautogui.leftClick()
    time.sleep(1)
    pyautogui.moveTo(833, 421)
    pyautogui.leftClick()

---
+++ automaticokey+mosue.py
@@ -0,0 +1,27 @@
+import pyautogui
+import time
+import threading
+
+
+pyautogui.moveTo(519,235)
+pyautogui.leftClick()
+pyautogui.press('down')
+pyautogui.press('space')
+pyautogui.press('space')
+pyautogui.press('down')
+pyautogui.press('space')
+pyautogui.press('space')
+pyautogui.press('down')
+pyautogui.press('space')
+pyautogui.press('space')
+pyautogui.press('down')
+pyautogui.press('space')
+pyautogui.press('space')
+time.sleep(1)
+pyautogui.moveTo(796, 439)
+pyautogui.leftClick()
+time.sleep(1)
+pyautogui.moveTo(833, 421)
+pyautogui.leftClick()
+
+i = i+1
